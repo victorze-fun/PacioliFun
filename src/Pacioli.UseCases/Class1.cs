f16949ce1d9ee4ac47464f0d7039f925c378d287
Victor <victorze@protonmail.com>
2021-07-01 07:33:37 -0500

    Add UseCases project

---
+++ src/Pacioli.UseCases/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Pacioli.UseCases
+{
+    public class Class1
+    {
+    }
+}
--- test/Pacioli.Entities.Tests/TransactionTest.cs
+++ test/Pacioli.Entities.Tests/TransactionTest.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xunit;
+﻿using Xunit;
 
 namespace Pacioli.Entities.Tests
 {
@@ -34,7 +29,6 @@ namespace Pacioli.Entities.Tests
 
             Assert.False(transaction.Balance());
         }
-
 
         [Fact]
         void ThreeEntries_Balance()
